31abcf12c23a074f8ef19c25ac0ea84578701def
Vasiliy Avdiushkin <vasiliy.avdiushkin@gmail.com>
2011-06-02 15:25:36 +0400

    new design, refreshes

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Simple python script, which collects RSSI information about wifi access points a
 For now - just run this script and view the result. Works on all Linux distros, where NetworkManager can be found.
 ##Commit hist
     02.06.2011: First commit - a printed list [AP: RSSI]
+    02.06.2011: Now refreshes on every signal change, completely new design
--- list_rssi.py
+++ list_rssi.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python
 #-*- encoding: utf-8 -*-
 import dbus
-bus = dbus.SystemBus()
-NM = 'org.freedesktop.NetworkManager'
-nm = bus.get_object(NM, '/org/freedesktop/NetworkManager')
-devlist = nm.GetDevices(dbus_interface = NM)
-wifiadl = []
-for i in devlist:
-    if bus.get_object(NM, i).Get(NM+'.Device', 'DeviceType', dbus_interface = 'org.freedesktop.DBus.Properties') == 2:
-        wifiadl.append(bus.get_object(NM, i))
+import gobject
+import os
+from dbus.mainloop.glib import DBusGMainLoop
 
-apl = [i.GetAccessPoints(dbus_interface = NM+'.Device.Wireless') for i in wifiadl]
+class WiFiList():
+    def __init__(self):
+        self.bus = dbus.SystemBus()
+        self.NM = 'org.freedesktop.NetworkManager'
+        self.bus.add_signal_receiver(self.handle_change, None, self.NM + '.AccessPoint', None, None)
+        nm = self.bus.get_object(self.NM, '/org/freedesktop/NetworkManager')
+        self.devlist = nm.GetDevices(dbus_interface = self.NM) 
+        self.rssid = {}
 
-rssid = {}
-for ads in apl:
-    for i in ads:
-        ssid = bus.get_object(NM, i).Get(NM+'.AccessPoint', 'Ssid', dbus_interface = 'org.freedesktop.DBus.Properties')
-        strength = bus.get_object(NM, i).Get(NM+'.AccessPoint', 'Strength', dbus_interface = 'org.freedesktop.DBus.Properties')
-        rssid["".join(["%s" % k for k in ssid])] =  int(strength)
+    def __repr__(self):
+        return "\n".join(["%20s: %5d" % (k, j) for k, j in self.rssid.items()])
 
-print "\n".join(["%20s: %5d" % (k, j) for k, j in rssid.items()])
+    def dbus_get_property(self, prop, member, proxy):
+        return proxy.Get(self.NM+'.' + member, prop, dbus_interface = 'org.freedesktop.DBus.Properties')
+
+    def repopulate_ap_list(self):
+        apl = []
+        res = []
+        for i in self.devlist:
+            tmp = self.bus.get_object(self.NM, i)
+            if self.dbus_get_property('DeviceType', 'Device', tmp) == 2:
+                apl.append(self.bus.get_object(self.NM, i).GetAccessPoints(dbus_interface = self.NM+'.Device.Wireless'))
+        for i in apl:
+            for j in i:
+                res.append(self.bus.get_object(self.NM, j))
+        return res
+    
+    def form_rssi_dic(self):
+        for i in self.repopulate_ap_list():
+            ssid = self.dbus_get_property('Ssid', 'AccessPoint', i)
+            strength = self.dbus_get_property('Strength', 'AccessPoint', i);
+            self.rssid["".join(["%s" % k for k in ssid])] =  int(strength)
+
+    def handle_change(self, kwargs = None):
+        self.form_rssi_dic()
+        os.system('clear')
+        print self
+
+if __name__ == '__main__':
+    loop = gobject.MainLoop()
+    DBusGMainLoop(set_as_default=True)
+    wfl = WiFiList()
+    wfl.form_rssi_dic()
+    print wfl
+    loop.run()
